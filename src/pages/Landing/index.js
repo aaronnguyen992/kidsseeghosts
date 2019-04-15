60e6631a9f3ec259d8e62d9ef5db59e9d906ee88
Aaron T Nguyen <aaronnguyen992@gmail.com>
2019-04-15 13:25:52 -0700

    tracklist json map created

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
+import Tracklist from '../../components/Tracklist';
+import tracklist from '../../components/Tracklist/tracklist.json';
+
 const Landing = () => {
     return (
         <div className="landing">
             <div className="landing__chaos">混沌</div>
             <div className="landing__kids-see-ghosts">KIDS SEE GHOSTS</div>
-            <ol className="landing__tracklist">
-                <li className="landing__track-1">Feel The Love <span className="landing__track-1-ft">ft. Pusha T</span></li>
-                <li className="landing__track-2">Fire</li>
-                <li className="landing__track-3">4th Dimension <span className="landing__track-3-ft">ft. Louis Prima</span></li>
-                <li className="landing__track-4">Freeee (Ghost Town, Pt. 2) <span className="landing__track-4-ft">ft. Ty Dolla Sign</span></li>
-                <li className="landing__track-5">Reborn</li>
-                <li className="landing__track-6">Kids See Ghosts <span>ft. Yasiin Bey</span></li>
-                <li className="landing__track-7">Cudi Montage</li>
-            </ol>
+            <div>
+            {
+                tracklist.map((track, i) => (
+                    <Tracklist 
+                        title={track.title}
+                        feature={track.feature}
+                        spotify={track.spotify}
+                        apple={track.apple}
+                        tidal={track.tidal}
+                        google={track.google}
+                        key={track.title}
+                        i={i}
+                    />
+                ))
+            }
+            </div>
         </div>
     )
 }
