8fa138116d3d58fe36a12dba3fd4f2b95195d947
Aaron T Nguyen <aaronnguyen992@gmail.com>
2019-04-16 16:00:09 -0700

    custom cursor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,71 @@
 import React from 'react';
-import Landing from './pages/Landing'
+import TweenMax from 'gsap/TweenMax';
+import Landing from './pages/Landing';
 
 import './sass/main.scss';
 
-const App = () => {
-  return (
-    <div className="main-page">
-      <Landing />
-    </div>
-  );
+var cursor, follower;
+var posX = 0,
+    posY = 0,
+    mouseX = 0,
+    mouseY = 0;
 
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.animateCursor = this.animateCursor.bind(this);
+   
+  }
+
+  componentDidMount() {
+    cursor = document.querySelector('.cursor');
+    follower = document.querySelector('.cursor-follower');
+
+    window.addEventListener("mousemove", e => {
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+    });
+
+    this.animateCursor();
+
+    console.log(cursor);
+  }
+
+  animateCursor() {
+    
+    TweenMax.to({}, 0.016, {
+      repeat: -1,
+      onRepeat: function() {
+        posX += (mouseX - posX) / 5;
+        posY += (mouseY - posY) / 5;
+        
+        TweenMax.set(follower, {
+            css: {    
+            left: posX - 18,
+            top: posY - 18
+            }
+        });
+        
+        TweenMax.set(cursor, {
+            css: {    
+            left: mouseX,
+            top: mouseY
+            }
+        });
+      }
+    });
+  }
+
+  render(){
+    return (
+      <div className="main-page">
+        <div className="cursor"></div>
+        <div className="cursor-follower"></div>
+        <Landing />
+      </div>
+    );
+  }
 }
 
 export default App;
