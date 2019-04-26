e76162444e9d0c9950b9eede92256af3ff9c186a
Aaron T Nguyen <aaronnguyen992@gmail.com>
2019-04-26 13:33:24 -0700

    image hover effect

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -3,29 +3,70 @@ import React from 'react';
 import Tracklist from '../../components/Tracklist';
 import tracklist from '../../components/Tracklist/tracklist.json';
 
-const Landing = () => {
-    return (
-        <div className="landing">
-            <div className="landing__chaos">混沌</div>
-            <div className="landing__kids-see-ghosts">KIDS SEE GHOSTS</div>
-            <div>
-            {
-                tracklist.map((track, i) => (
-                    <Tracklist 
-                        title={track.title}
-                        feature={track.feature}
-                        spotify={track.spotify}
-                        apple={track.apple}
-                        tidal={track.tidal}
-                        google={track.google}
-                        key={track.title}
-                        i={i}
-                    />
-                ))
-            }
+class Landing extends React.Component {
+
+    componentDidMount() {    
+        this.animateBlur();
+    }
+
+    
+
+    animateBlur(){
+        const album = document.querySelector('.landing__album');
+        const maskElement = document.querySelector('#mask1 circle');
+        var upX, upY, mask;
+        
+        album.addEventListener("mouseenter", event => {
+            album.addEventListener("mousemove", event => {
+                let upX = event.clientX;
+                let upY = event.clientY;
+                let mask = maskElement;
+                mask.setAttribute("cy", (upY - 60));
+                mask.setAttribute("cx", (upX - 480));
+            })
+        })
+        
+        album.addEventListener("mouseleave", event => {
+            
+        })
+        
+    }
+    render(){
+        return (
+            <div className="landing">
+                <div className="landing__chaos">混沌</div>
+                <div className="landing__kids-see-ghosts">KIDS SEE GHOSTS</div>
+                <div className="landing__album">
+                    <svg className='landing__album--blur' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink' width="100%">
+                        <image filter='url(#filter2)' width='100%' height='100%' />
+                        <filter id='filter2'>
+                            <feGaussianBlur stdDeviation='5' />
+                        </filter>
+                        <mask id='mask1'>
+                            <circle cx='0' cy='0' r='75' fill='#fff' filter='url(#filter2)'/>
+                        </mask>
+                        <image className="landing__album--art" xlinkHref="/assets/AlbumCover.jpg" alt="Album Cover" width="100%" height="100%" mask='url(#mask1)' />
+                    </svg>
+                </div>
+                <div className="landing__tracklist">
+                {
+                    tracklist.map((track, i) => (
+                        <Tracklist 
+                            title={track.title}
+                            feature={track.feature}
+                            spotify={track.spotify}
+                            apple={track.apple}
+                            tidal={track.tidal}
+                            google={track.google}
+                            key={track.title}
+                            i={i}
+                        />
+                    ))
+                }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Landing;
