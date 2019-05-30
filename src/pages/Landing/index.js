8f943c4e287ead706869f6c5f634989b2a6298bb
Aaron T Nguyen <aaronnguyen992@gmail.com>
2019-05-30 15:24:07 -0700

    added event handlers for tracklist

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -4,12 +4,19 @@ import Tracklist from '../../components/Tracklist';
 import tracklist from '../../components/Tracklist/tracklist.json';
 
 class Landing extends React.Component {
+    state = {
+        didHover: false
+    }
+
+    handleHover() {
+        this.setState( state => ({
+            didHover: !state.didHover
+        }))
+    }
 
     componentDidMount() {    
         this.animateBlur();
     }
-
-    
 
     animateBlur(){
         const album = document.querySelector('.landing__album');
@@ -19,16 +26,21 @@ class Landing extends React.Component {
             let upX = event.clientX;
             let upY = event.clientY;
             let mask = maskElement;
-            mask.setAttribute("cy", (upY));
+            mask.setAttribute("cy", (upY - 200));
             mask.setAttribute("cx", (upX));
         })
         
     }
     render(){
+        const didHover = this.state.didHover ? 'hover' : ''
+
         return (
             <div className="landing">
                 <div className="landing__chaos">混沌</div>
-                <div className="landing__kids-see-ghosts">KIDS SEE GHOSTS</div>
+                <div className="landing__kids-see-ghosts" 
+                     >
+                     KIDS SEE GHOSTS
+                </div>
                 <div className="landing__album">
                     <svg className='landing__album--blur' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink' width="100%">
                         <image filter='url(#filter2)' width='100%' height='100%' />
@@ -38,10 +50,20 @@ class Landing extends React.Component {
                         <mask id='mask1'>
                             <circle cx='0' cy='0' r='71' fill='#fff' filter='url(#filter2)'/>
                         </mask>
-                        <image className="landing__album--art" xlinkHref="/assets/AlbumCover.jpg" alt="Album Cover" width="100%" height="90%" mask='url(#mask1)' />
+                        <image 
+                            onMouseEnter={() => this.handleHover()} 
+                            onMouseLeave={() => this.handleHover()} 
+                            className="landing__album--art" 
+                            xlinkHref="/assets/AlbumCover.jpg" 
+                            alt="Album Cover" 
+                            width="100%" 
+                            height="90%" 
+                            mask='url(#mask1)' 
+                        />
                     </svg>
                 </div>
-                <div className="tracklist">
+
+                <div className={`tracklist ${didHover}`}>
                 {
                     tracklist.map((track, i) => (
                         <Tracklist 
