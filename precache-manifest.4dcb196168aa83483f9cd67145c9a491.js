2e2d14679de86e20238e702fe168851366ffe5f8
Aaron T Nguyen <aaronnguyen992@gmail.com>
2019-08-17 16:35:48 -0700

    Updates

---
+++ precache-manifest.4dcb196168aa83483f9cd67145c9a491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb6ce4673d883804959b",
+    "url": "/kidsseeghosts/static/js/runtime~main.0ea448de.js"
+  },
+  {
+    "revision": "4787105273e0efab7366",
+    "url": "/kidsseeghosts/static/js/main.733b27a7.chunk.js"
+  },
+  {
+    "revision": "f3f17cdb2eeb6aac7300",
+    "url": "/kidsseeghosts/static/js/2.d6bd287f.chunk.js"
+  },
+  {
+    "revision": "4787105273e0efab7366",
+    "url": "/kidsseeghosts/static/css/main.16d289c0.chunk.css"
+  },
+  {
+    "revision": "93f0fd8cd1ec75c21e15487a1e8eed00",
+    "url": "/kidsseeghosts/index.html"
+  }
+];
