8e837b4a4dd915a35f5e42d26a08ec54a95f2ca2
Aaron T Nguyen <aaronnguyen992@gmail.com>
2019-08-17 19:27:55 -0700

    added personal credits

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -72,6 +72,9 @@ class Landing extends React.Component {
                             </a>
                         </div>
                     </div>
+                    <div className="landing__credits">
+                        <p className="landing__author">Designed and Developed by Aaron Nguyen</p>
+                    </div>
                 </div>
                 <div className="landing__album">
                     <svg className='landing__album--blur' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink' width="100%">
